7f7282b0327432571b9cd56e7ac4a7591c368236
superymk <superymk@875c5e1d-5f53-0410-a04a-fb0481a72a73>
2009-03-11 15:10:01 +0000

    Modify Enlist Wizard to support user specified razzle environment setup.

--- trunk/Framework/MadDog/src/Tools/RazzleTemplate.bat
+++ trunk/Framework/MadDog/src/Tools/RazzleTemplate.bat
@@ -1,21 +1,25 @@
 @echo off
-:Part 1. Set environment variables.
+@rem Part 1. Set environment variables.
 :TODO - This part should be moved to the environment later
 set ENLISTMENT_PROJ_ROOT=%%ENLISTMENT_PROJ_ROOT%%
 set RAZZLE_INDEX=%%RAZZLE_INDEX%%
 set DDKHome=%%WINDDK_HOME%%
+set CURRENT_USER=%%CURRENTUSER%%
 set _NT_SYMBOL_PATH=%_NT_SYMBOL_PATH%;%ENLISTMENT_PROJ_ROOT%\bin\Framework\bin\i386
 set path=%path%;%ENLISTMENT_PROJ_ROOT%\bin\Tools\
 
-:Step 1.1 Call Public\env.bat to setup other env vars.
+@rem Step 1.1 Call Public\env.bat to setup other env vars.
 call %ENLISTMENT_PROJ_ROOT%\src\Programmers\Public\env.bat
 
-:Step 2. Set command alias
+@rem Step 1.2 Call User\env.bat to setup other env vars.
+call %ENLISTMENT_PROJ_ROOT%\src\Programmers\%CURRENT_USER%\env.bat
+
+@rem Step 2. Set command alias
 doskey src=cd /d %ENLISTMENT_PROJ_ROOT%\src\Framework
 doskey bin=cd /d %ENLISTMENT_PROJ_ROOT%\bin\Framework
 doskey test=cd /d %ENLISTMENT_PROJ_ROOT%\test
 
-:Step 3. Startup build env.
+@rem Step 3. Startup build env.
 %windir%\System32\cmd.exe /k %DDKHome%\bin\setenv.bat %DDKHome% %1 %2
 
 src
