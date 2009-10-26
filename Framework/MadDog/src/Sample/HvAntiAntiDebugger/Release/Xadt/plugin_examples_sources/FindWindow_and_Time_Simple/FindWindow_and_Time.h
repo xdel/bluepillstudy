25568be6b650a1427147ac05c1130594f14ed14e
Superymk <Superymk@875c5e1d-5f53-0410-a04a-fb0481a72a73>
2009-10-26 05:41:10 +0000

    Cut the anti-anti debugger project

--- Framework/MadDog/src/Sample/HvAntiAntiDebugger/Release/Xadt/plugin_examples_sources/FindWindow_and_Time_Simple/FindWindow_and_Time.h
+++
@@ -1,9 +0,0 @@
-#include "xADT_PDK.h"
-
-EXPORT Result tst_FindWindow(char *message);
-EXPORT char* tst_FindWindow_description();
-EXPORT char* tst_FindWindow_name();
-
-EXPORT Result tst_GetSystemTime(char *message);
-EXPORT char* tst_GetSystemTime_description();
-EXPORT char* tst_GetSystemTime_name();
--- Framework/MadDog/src/Sample/HvAntiAntiDebugger/Release/Xadt/plugin_examples_sources/SICE_Tricks/SICETricks.h
+++
@@ -1,6 +0,0 @@
-#include "xADT_PDK.h"
-
-
-EXPORT Result tst_SICETricks(char *message);
-EXPORT char* tst_SICETricks_description();
-EXPORT char* tst_SICETricks_name();
