ce33f8833632c2f716b99e4bc78ee9afda6bca50
Superymk <Superymk@875c5e1d-5f53-0410-a04a-fb0481a72a73>
2009-10-26 05:41:10 +0000

    Cut the anti-anti debugger project

--- trunk/Framework/MadDog/src/Sample/HvAntiAntiDebugger/Release/Xadt/plugin_examples_sources/INVALID_HANDLE exception/Invalid_HandleException.h
+++
@@ -1,7 +0,0 @@
-#include "xADT_PDK.h"
-
-#include <afx.h>
-
-EXPORT Result tst_Invalid_HandleException(char *message);
-EXPORT char* tst_Invalid_HandleException_description();
-EXPORT char* tst_Invalid_HandleException_name();
--- trunk/Framework/MadDog/src/Sample/HvAntiAntiDebugger/Release/Xadt/plugin_examples_sources/ParentProcess/ParentProcess.h
+++
@@ -1,5 +0,0 @@
-#include "xADT_PDK.h"
-
-EXPORT Result tst_ParentProcess(char *message);
-EXPORT char* tst_ParentProcess_description();
-EXPORT char* tst_ParentProcess_name();
